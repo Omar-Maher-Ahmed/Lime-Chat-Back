9203b8573af2f0a3db7db2ab781fe8d88b684a73
Omar-Maher-Ahmed <youremail@example.com>
2025-07-11 14:23:39 +0300

    feat: Refactor project structure and database connection
    This commit refactors the project structure and updates the database connection logic.
    
    - Moves the main server entry point from `server.js` to `src/server/index.js`.
    - Updates the `start` and `dev` scripts in `package.json` to reflect the new server entry point.
    - Deletes the `server.js` file.
    - Deletes the `src/utils/database.js` file.

---
+++ src/server/bootstrap.js
@@ -0,0 +1,23 @@
+import { connectDB } from "../db/database.js";
+import cors from 'cors';
+import authRoutes from "../routes/auth.routes.js";
+import userRoutes from "../routes/user.routes.js";
+import roomRoutes from "../routes/room.routes.js";
+import messageRoutes from "../routes/message.routes.js";
+import callRoutes from "../routes/call.routes.js";
+
+
+const bootstrap = (app) => {
+    app.use(cors());
+
+    connectDB()
+
+    app.use('/api/auth', authRoutes);
+    app.use('/api/user', userRoutes);
+    app.use('/api/room', roomRoutes);
+    app.use('/api/message', messageRoutes);
+    app.use('/api/call', callRoutes);
+
+}
+
+export default bootstrap
---
+++ src/server/index.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import express from "express";
+import http from 'http';
+import { Server } from 'socket.io';
+import bootstrap from "./bootstrap.js";
+import initSocket from "../socket/socket.js";
+
+
+const app = express();
+
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+});
+
+bootstrap(app);
+
+initSocket(io);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
+server.listen(5001, () => console.log('Server running on port 5001'));
