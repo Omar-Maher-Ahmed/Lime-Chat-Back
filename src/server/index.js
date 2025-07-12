cecf5192dbbb85e26560b3c5b8bfe64e476a0f6e
Omar-Maher-Ahmed <youremail@example.com>
2025-07-12 15:55:46 +0300

    fix: Update server listening logic to use the correct port variable

--- src/server/index.js
+++ src/server/index.js
@@ -26,5 +26,5 @@ initSocket(io);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-server.listen(5001, () => console.log('Server running on port 5001'));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(5001, () => console.log('Server running on port 5001'));
