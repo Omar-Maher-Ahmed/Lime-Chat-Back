4409a5a36f00d81a7886ff6f04c8dbd3351242af
Omar-Maher-Ahmed <youremail@example.com>
2025-07-12 15:07:38 +0300

    refactor: Use environment variable for server port configuration

--- src/server/index.js
+++ src/server/index.js
@@ -25,5 +25,7 @@ bootstrap(app);
 
 // initSocket(io);
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // server.listen(5001, () => console.log('Server running on port 5001'));
