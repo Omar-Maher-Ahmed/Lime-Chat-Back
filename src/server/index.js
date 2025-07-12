84761f659bcc39bf9d20095fcf07ecee14fd3815
Omar-Maher-Ahmed <youremail@example.com>
2025-07-12 15:07:00 +0300

    refactor: Comment out unused WebSocket server code for clarity

--- src/server/index.js
+++ src/server/index.js
@@ -7,9 +7,6 @@ import cors from 'cors';
 import express from "express";
 import http from 'http';
 import { Server } from 'socket.io';
-import { WebSocketServer } from 'ws';
-import path from 'path';
-
 
 import bootstrap from "./bootstrap.js";
 import initSocket from "../socket/socket.js";
@@ -19,14 +16,14 @@ app.use(cors());
 
 app.use(express.json());
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 // const wss = new WebSocketServer({ server })
-const io = new Server(server);
+// const io = new Server(server);
 
 
 bootstrap(app);
 
-initSocket(io);
+// initSocket(io);
 
 app.listen(5000, () => console.log('Server running on port 5000'));
-server.listen(5001, () => console.log('Server running on port 5001'));
+// server.listen(5001, () => console.log('Server running on port 5001'));
