86badb30ebec7b02f5095a2cd255c71ddc8ae554
Omar-Maher-Ahmed <youremail@example.com>
2025-07-12 15:36:09 +0300

    refactor: Uncomment necessary server and socket initialization code for proper functionality

--- src/server/index.js
+++ src/server/index.js
@@ -5,27 +5,26 @@ dotenv.config();
 import cors from 'cors';
 
 import express from "express";
-// import http from 'http';
-// import { Server } from 'socket.io';
+import http from 'http';
+import { Server } from 'socket.io';
 
 import bootstrap from "./bootstrap.js";
-// import initSocket from "../socket/socket.js";
+import initSocket from "../socket/socket.js";
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 
-// const server = http.createServer(app);
-// const wss = new WebSocketServer({ server })
-// const io = new Server(server);
+const server = http.createServer(app);
+const io = new Server(server);
 
 
 bootstrap(app);
 
-// initSocket(io);
+initSocket(io);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// server.listen(5001, () => console.log('Server running on port 5001'));
+server.listen(5001, () => console.log('Server running on port 5001'));
